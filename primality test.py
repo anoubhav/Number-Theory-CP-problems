20b542625306d22e5177e7c8e16aa81ce3efdc63
Anoubhav Agarwaal <anoubhav.agarwaal@gmail.com>
2020-07-09 16:03:54 +0530

    Faster primality test and sieve optimisations in python 3

--- primality test.py
+++ primality test.py
@@ -1,10 +1,6 @@
 # Check if the number is prime
 from sys import stdin
 
-# n = int(stdin.readline())
-# n = 12207031
-n = 305175781
-
 def naive(n):
     # O(n) time complexity; O(1) space
     if n==1: return False
@@ -14,19 +10,50 @@ def naive(n):
             return False
     return True
 
-def better(n):
+def good(n):
     # O(sqrt n) time complexity; O(1) space
     if n ==1: return False
     i = 2
+    # a number has at most 1 prime factor greater than sqrt n.
     while i*i<=n:
         if n%i == 0: return False
         i+=1
 
     return True
 
+def better(n):
+    if n ==1: return False
+    if n == 2: return True
+    # only 2 is even prime factor
+    i = 3
+    while i*i<=n:
+        if n%i == 0: return False
+        i+=2
+    return True
+
+def best(num):
+    if num == 1: return False
+    elif num < 4: return True
+    elif num%2 == 0 or num%3 == 0: return False
+    else:
+        for i in range(5, int(num**0.5) + 1, 6):
+            if num%i==0:return False
+            if num%(i+2)==0:return False
+        return True
+
+# n = 305175781
+n = 1000000000100011
+# True 5.341723203659058
+# True 2.703773260116577
+# True 1.1240077018737793
+
 from time import time
+# t = time()
+# print(naive(n), time() - t) # 20 s for 305175781
 t = time()
-print(naive(n), time() - t) # 20 s
+print(good(n), time() - t)  # 0.002 s
 t = time()
-print(better(n), time() - t) # 0.04 s
+print(better(n), time() - t) # 0. 00099
+t = time()
+print(best(n), time() - t) # 0. 00099
 
--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -3,20 +3,76 @@
 # Then using basic primality test each time will yield a complexity of O(q*sqrt(n)). This will give TLE. 
 # So instead we can precompute using sieve for all N numbers.
 
-n = 10**4
 
-is_prime = [1]*(n + 1)
-is_prime[0], is_prime[1] = 0, 0
+# The idea behind is this: A number is prime, if none of the smaller prime numbers divides it. Since we iterate over the prime numbers in order, we already marked all numbers, who are divisible by at least one of the prime numbers, as divisible. Hence if we reach a cell and it is not marked, then it isn't divisible by any smaller prime number and therefore has to be prime.
 
-for num in range(2, n+1):
-    if num*num>n:
-        break
 
-    if is_prime[num] == 0:
-        continue
+# If the current number i is a prime number, it marks all numbers that are multiples of i as composite numbers, starting from i^2. This is already an optimization over naive way of implementing it, and is allowed as all smaller numbers that are multiples of i necessary also have a prime factor which is less than i, so all of them were already sifted earlier.
 
-    for mult in range(2*num, n+1, num):
-        is_prime[mult] = 0
+# Uses only odd numbers and a concept called bytearray
+def sieve_fastest(lim):
+    from itertools import compress
+    # Find all primes upto n  (including n). NOTE: it does not include 2; To get list, convert the return value to list.
+    BA = bytearray
+    n = (lim-1)//2
+    prime = BA([1])*(n+1)
+    prime[0] = 0 # 2*0+1 = 1 n'est pas premier
 
-primes = [i for i in range(n+1) if is_prime[i]]
-print(*primes)
+    for i in range((int(lim**0.5)+1)//2):
+        if prime[i]:
+            p = 2*i+1 # p is prime
+            i2 = i*(i+1)<<1
+            prime[i2::p]=BA(1+ (n-i2)//p )
+    return compress(range(1,lim+1,2),prime)
+
+# Src: https://projecteuler.net/overview=010
+# An easy optimisation of the sieve is suggested by the discrimination of odd and even numbers. Apart from 2, all that the even numbers do is being crossed out once and occupying space.  If we get rid of the even numbers, we save (limit − 2)/2 crossings out and, more importantly, we use only half the memory. 
+def sieve_optimized(n):
+    # Find all primes upto n  (including n)
+    sievebound = (n-1)//2
+    sieve = [False]*(sievebound + 1)
+    sieve[0] = True
+    crosslimit = (int(sievebound**0.5) - 1)
+    for i in range(1, crosslimit + 1):
+        if not sieve[i]:
+            for j in range(2*i*(i+1), sievebound + 1, 2*i+1):
+                sieve[j] = True
+    
+    return [2] + [2*i+1 for i in range(1, sievebound+1) if not sieve[i]]
+
+def sieve(n):
+    is_prime = [1]*(n + 1)
+    is_prime[0], is_prime[1] = 0, 0
+
+    for num in range(2, n+1):
+        if num*num>n:
+            break
+
+        if is_prime[num]:
+            for mult in range(num*num, n+1, num):
+                is_prime[mult] = 0
+
+    primes = [i for i in range(n+1) if is_prime[i]]
+    return primes
+
+from time import time
+n = 10000000
+
+# 200ms for 10^7
+t = time()
+print(len(list(sieve_fastest(n))) + 1) # it does not include 2.
+print(time() - t)
+
+# 920ms
+t = time()
+print(len(sieve_optimized(n)))
+print(time() - t)
+
+#1830ms
+t = time()
+print(len(sieve(n)))
+print(time() - t)
+
+
+
+# Code from https://projecteuler.net/overview=010
